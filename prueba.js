c99a63faf95961a93914090c7c50a918eb85a643
Ramon Escobar <rescobar@saltala.com>
2019-12-08 19:42:26 -0300

    tarea para la casa

---
+++ prueba.js
@@ -0,0 +1,54 @@
+let users = [
+    {
+        name: 'Ramón',
+        lastname: 'Escobar'
+    },
+    {
+        name: 'Camila',
+        lastname: 'Vallejos'
+    },
+    {
+        name: 'Miguel',
+        lastname: 'Calluqueo'
+    },
+    {
+        name: 'Gabriel',
+        lastname: 'Condeza'
+    },
+    {
+        name: 'Francisco',
+        lastname: 'Castillo'
+    },
+]
+//1.- Imprime en un console.log() tu nombre y apellido
+//2.- Asigna tu edad al objeto
+
+mystery = (a, b) => {
+    return a == b == 2
+}
+
+console.log(mystery(2, 2))
+//3.- ¿Que retorna la funcion mystery?
+
+const arr = [
+    {
+        name: 'Jose',
+        age: 20
+    },
+    {
+        name: 'Luis',
+        age: 25
+    },
+    {
+        name: 'Alberto',
+        age: 40
+    }
+]
+//4.- Crea una funcion que retorne los usuarios
+//que tengan mas de 30 años
+
+//5.- Crea una funcion que recorra del 1 al 100 y verifique
+//si el numero es divisible entre 3, 5 y 3 y 5,
+//si el número es divisible entre 3 imprime 'Fizz'
+//si el número es divisible entre 5 imprime 'Buzz'
+//si el número es divisible entre 3 Y 5 imprime 'FizzBuzz'
